783f00f2d768fb5663695bd16fdf57101d7e63cd
mustafaturkdag <mustafaturkdag@gmail.com>
2023-10-08 15:23:57 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const options = {
+    method: "GET",
+    url: "https://text-translator2.p.rapidapi.com/getLanguages",
+    headers: {
+        'X-RapidAPI-Key': '4056d6deddmsha81d56f921ca289p1648cbjsn7eef52f3f973',
+        'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+      }
+    };
